b4014342e37eb42cf87f16151766a0130554de7e
Amanda Shuman <amanda.n.shuman@gmail.com>
2021-08-15 19:03:20 -0400

    Thought processes

--- war.py
+++ war.py
@@ -1,8 +1,9 @@
 import random
 
-num_cards = ["1", "2", "3", "4", "5", "6", "7",
+num_cards = ["2", "3", "4", "5", "6", "7",
              "8", "9", "10", "Jack", "Queen", "King", "Ace"]
 suits = ["Hearts", "Spades", "Diamonds", "Clubs"]
+points = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
 
 #build deck of cards
 hearts = []
@@ -29,3 +30,23 @@ print(player1_deck)
 print(player2_deck)
 
 #time to play!
+#card count
+player1_score = len(player1_deck)
+player2_score = len(player2_deck)
+
+#draw 1 card from each pile until 1 person wins
+card_num = 0
+while player1_score < 52 and player2_score < 52:
+    played_cards = []
+    #print players' scores
+
+
+    #pull a card from each player
+    player1 = player1_deck[card_num]
+    player2 = player2_deck[card_num]
+    print(f"Player 1 has played the {player1}.")
+    print(f"player 2 has played the {player2}.")
+
+    #check to see who wins the match
+    
+    #add cards into a new pile and append it to the end of the winner's deck
