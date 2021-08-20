a1ca1be980bee23c1d91bae9d6461ad502cba129
Amanda Shuman <amanda.n.shuman@gmail.com>
2021-08-20 18:30:46 -0400

    assigning points

--- card_pkg/card_functions.py
+++ card_pkg/card_functions.py
@@ -12,12 +12,50 @@ def full_deck():
             card = num_cards[i] + " of " + suits[j]
             deck.append(card)
             random.shuffle(deck)
-    print(deck)
-'''
+    return deck
+
 def sublist(suit):
-    full_deck()
-    sub_list = [k for k in full_deck() if 'suit' in k]
-    print(sub_list)
-sublist("Hearts")
-'''
-full_deck()
+    global deck
+    deck = full_deck()
+    hearts = []
+    spades = []
+    diamonds = []
+    clubs = []
+    for i in range(13):
+        heart_card = num_cards[i]+" of "+suits[0]
+        hearts.append(heart_card)
+        spade_card = num_cards[i]+" of "+suits[1]
+        spades.append(spade_card)
+        diamond_card = num_cards[i]+" of "+suits[2]
+        diamonds.append(diamond_card)
+        club_card = num_cards[i]+" of "+suits[3]
+        clubs.append(club_card)
+
+def card_points(card):
+    if card[0] == "2":
+        points = 2
+    elif card[0] == "3":
+        points = 3
+    elif card[0] == "4":
+        points = 4
+    elif card[0] == "5":
+        points = 5
+    elif card[0] == "6":
+        points = 6
+    elif card[0] == "7":
+        points = 7
+    elif card[0] == "8":
+        points = 8
+    elif card[0] == "9":
+        points = 9
+    elif card[0] == "10":
+        points = 10
+    elif card[0] == "J":
+        points = 11
+    elif card[0] == "Q":
+        points = 12
+    elif card[0] == "K":
+        points = 13
+    else:
+        points = 14
+    return points
--- war.py
+++ war.py
@@ -1,33 +1,9 @@
 import random
-
-num_cards = ["2", "3", "4", "5", "6", "7",
-             "8", "9", "10", "Jack", "Queen", "King", "Ace"]
-suits = ["Hearts", "Spades", "Diamonds", "Clubs"]
-points = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-
-#build deck of cards
-hearts = []
-spades = []
-diamonds = []
-clubs = []
-for i in range(13):
-    heart_card = num_cards[i]+" of "+suits[0]
-    hearts.append(heart_card)
-    spade_card = num_cards[i]+" of "+suits[1]
-    spades.append(spade_card)
-    diamond_card = num_cards[i]+" of "+suits[2]
-    diamonds.append(diamond_card)
-    club_card = num_cards[i]+" of "+suits[3]
-    clubs.append(club_card)
-
-deck_of_cards = hearts + diamonds + spades + clubs
+from card_pkg import card_functions
 
 #shuffle deck and separate into 2 piles for each player
-player1_deck = random.sample(deck_of_cards, 26)
-player2_deck = set(deck_of_cards) - set(player1_deck)
-
-print(player1_deck)
-print(player2_deck)
+player1_deck = random.sample(card_functions.full_deck(), 26)
+player2_deck = list(set(card_functions.full_deck()) - set(player1_deck))
 
 #time to play!
 #card count
@@ -37,16 +13,24 @@ player2_score = len(player2_deck)
 #draw 1 card from each pile until 1 person wins
 card_num = 0
 while player1_score < 52 and player2_score < 52:
-    played_cards = []
     #print players' scores
-
+    print("\nPlayer 1 score: " + str(player1_score) + "   Player 2 score: " + str(player2_score))
 
     #pull a card from each player
     player1 = player1_deck[card_num]
     player2 = player2_deck[card_num]
     print(f"Player 1 has played the {player1}.")
     print(f"player 2 has played the {player2}.")
+    card_num += 1
+
+    #add cards into a new pile and append it to the end of the winner's deck
+    player1_deck.remove(player1)
+    player2_deck.remove(player2)
+    playing_pile = []
+    playing_pile.append(player1)
+    playing_pile.append(player2)
 
     #check to see who wins the match
-    
-    #add cards into a new pile and append it to the end of the winner's deck
+
+
+    break
