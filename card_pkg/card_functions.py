86dbaa29ad4e47b6beb08a8317412a65bb724264
Amanda Shuman <amanda.n.shuman@gmail.com>
2021-08-20 17:38:19 -0400

    making deck global

--- card_pkg/card_functions.py
+++ card_pkg/card_functions.py
@@ -3,13 +3,21 @@ import random
 num_cards = ["2", "3", "4", "5", "6", "7",
              "8", "9", "10", "Jack", "Queen", "King", "Ace"]
 suits = ["Hearts", "Spades", "Diamonds", "Clubs"]
+deck = []
 
 def full_deck():
-    deck = []
+    global deck
     for i in range(13):
         for j in range(4):
             card = num_cards[i] + " of " + suits[j]
             deck.append(card)
             random.shuffle(deck)
-    return deck
-
+    print(deck)
+'''
+def sublist(suit):
+    full_deck()
+    sub_list = [k for k in full_deck() if 'suit' in k]
+    print(sub_list)
+sublist("Hearts")
+'''
+full_deck()
